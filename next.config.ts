c961d8b66c08e8fb247e0c3fd90fc1edd583fd2e
[dyad] <shamedigital@gmail.com>
2025-08-08 22:15:53 +0100

    [dyad] Configured Next.js to allow images from imgur.com - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com', // Added imgur.com
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
