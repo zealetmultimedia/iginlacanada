cf9d4e55935fd420692bceb838c93000f0797cb2
[dyad] <shamedigital@gmail.com>
2025-08-10 14:39:11 +0100

    [dyad] Reordered sections on the homepage - wrote 1 file(s)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
         <Hero />
         <ContactBar />
         <Highlights />
-        <Videos />
         <Hosts />
+        <Videos />
         <Gallery />
         <Registration />
         <Location />
