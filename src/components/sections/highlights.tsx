4a8d7786a746d91031ac913ef8f300454aa99019
[dyad] <shamedigital@gmail.com>
2025-08-10 14:34:36 +0100

    Updated src/components/sections/highlights.tsx

--- src/components/sections/highlights.tsx
+++ src/components/sections/highlights.tsx
@@ -65,7 +65,7 @@ export default function Highlights() {
           </div>
           <div>
             <Image
-              src="https://i.imgur.com/PBMI6Zy.png"
+              src="https://i.imgur.com/Nd1PEVK.png"
               alt="Canada Invasion 2025 Event Poster"
               width={720}
               height={1080}
